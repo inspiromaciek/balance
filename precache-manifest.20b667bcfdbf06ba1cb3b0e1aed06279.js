7dc492d01608f96a720c63f9efcf1c6d39bbcc63
Maciej Dąbrowski <inspiromaciek@gmail.com>
2020-07-02 12:24:23 +0200

    Updates

---
+++ precache-manifest.20b667bcfdbf06ba1cb3b0e1aed06279.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5598607d7ac915b67407d97e38fd3e6d",
+    "url": "/balance/index.html"
+  },
+  {
+    "revision": "f1a898473497e0dabfe6",
+    "url": "/balance/static/css/main.b0ebf51b.chunk.css"
+  },
+  {
+    "revision": "9aa9d32b3d662d02012a",
+    "url": "/balance/static/js/2.20c7e5c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/balance/static/js/2.20c7e5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a898473497e0dabfe6",
+    "url": "/balance/static/js/main.74611bb5.chunk.js"
+  },
+  {
+    "revision": "455d365957e52a23a46c",
+    "url": "/balance/static/js/runtime-main.4aed1649.js"
+  },
+  {
+    "revision": "1d4b3159af137c6cea123c526a1141be",
+    "url": "/balance/static/media/leg.1d4b3159.png"
+  },
+  {
+    "revision": "433c6c1fcc487867ded9f8fe8e101f20",
+    "url": "/balance/static/media/shelter.433c6c1f.png"
+  },
+  {
+    "revision": "a4be31ebab95384d1364571362e06705",
+    "url": "/balance/static/media/tree.a4be31eb.png"
+  },
+  {
+    "revision": "92b66916fa667043a0a58c8c316f4097",
+    "url": "/balance/static/media/vacine.92b66916.png"
+  }
+]);

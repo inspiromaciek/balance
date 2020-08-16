61e32d0cc85d19c4f604a504ad640ec33c895583
inspiromaciek <59687429+inspiromaciek@users.noreply.github.com>
2020-08-16 23:09:42 +0200

    Add files via upload

---
+++ build/precache-manifest.9da2340e438c2d7806b0a7f4f6e99ae4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c4359fd51be3d6d81fd3b308c59019",
+    "url": "/balance/index.html"
+  },
+  {
+    "revision": "5944819541b32e1e4e1a",
+    "url": "/balance/static/css/main.7f74cabd.chunk.css"
+  },
+  {
+    "revision": "bbfb492fac486322c7ec",
+    "url": "/balance/static/js/2.44bf84f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/balance/static/js/2.44bf84f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5944819541b32e1e4e1a",
+    "url": "/balance/static/js/main.9f24c66a.chunk.js"
+  },
+  {
+    "revision": "455d365957e52a23a46c",
+    "url": "/balance/static/js/runtime-main.4aed1649.js"
+  },
+  {
+    "revision": "1d4b3159af137c6cea123c526a1141be",
+    "url": "/balance/static/media/leg.1d4b3159.png"
+  },
+  {
+    "revision": "433c6c1fcc487867ded9f8fe8e101f20",
+    "url": "/balance/static/media/shelter.433c6c1f.png"
+  },
+  {
+    "revision": "a4be31ebab95384d1364571362e06705",
+    "url": "/balance/static/media/tree.a4be31eb.png"
+  },
+  {
+    "revision": "92b66916fa667043a0a58c8c316f4097",
+    "url": "/balance/static/media/vacine.92b66916.png"
+  }
+]);
